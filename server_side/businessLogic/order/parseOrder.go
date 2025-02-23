4b2d7d87f0598c34094ea8689e741eb4ede81f4c
Leonov1995 <www.leonofff@yandex.ru>
2025-02-23 13:54:21 +0300

    add log to fix

--- server_side/businessLogic/order/parseOrder.go
+++ server_side/businessLogic/order/parseOrder.go
@@ -2,6 +2,9 @@ package order
 
 import (
 	"encoding/json"
+	"fmt"
+	"log"
+	"os"
 	"server/models"
 )
 
@@ -36,11 +39,17 @@ func decodeOrder(data []byte) (models.Order, error) {
 
 	totalCost := calculateTotalCost(bouquets)
 
+	paymentInfo := getPayInfo(currentOrderID)
+
+	isPaid := paymentInfo.IsPaid
+
 	order := models.Order{
 		ID:        currentOrderID,
 		List:      bouquets,
 		OrderCost: totalCost,
+		Payment:   isPaid,
 	}
+
 	return order, nil
 }
 
@@ -54,4 +63,26 @@ func calculateTotalCost(bouquets []models.Bouquet) (totalCost int) {
 		totalCost += bouquet.Cost
 	}
 	return
+}
+
+func getPayInfo(ID int) models.Payment {
+	fmt.Println(ID)
+
+	payInfo, err := os.ReadFile("payment1.json")
+	if err != nil {
+		log.Println(err)
+		return models.Payment{}
+	}
+
+	fmt.Println(payInfo)
+
+	var payment models.Payment
+
+	if err := json.Unmarshal(payInfo, &payment); err != nil {
+		fmt.Println(err)
+		return models.Payment{}
+	}
+	fmt.Println(payment)
+
+	return payment
 }
